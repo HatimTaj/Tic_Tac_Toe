ae75d6ca65542af85e72ed834339d0fad5d85580
HatimTaj <hatimtaj5306@gmail.com>
2025-03-26 23:56:10 +0530

    Some more changes

--- game.js
+++ game.js
@@ -1,17 +1,24 @@
 let boxes=document.querySelectorAll(".cell");
 let turn=document.querySelector("#status");
-let gameover=false; 
-let winner=null;
-let count=0;
-turn="X" ;
-function turnChange(){
-    if(turn==="X"){
-        boxes.forEach(box=>{
-            box.addEventListener("click",box.innerHTML="X");
-        }
-        turn="O";
-    }
-    else{
-        turn="X";
+turn = "X";
+let i=0;
+console.log(boxes)
+
+
+let check=()=>{
+for(let i=0;i<boxes.length;i++){
+    if(cell[i].innerHTML==""){
+        if(turn==="X"){
+            boxes[i].innerHTML="X";
+            turn="O";
+            turn.innerHTML="O's turn";  
+        } 
+        if(turn==="O"){
+            boxes[i].innerHTML="O";
+            turn="X";
+            turn.innerHTML="X's turn";  
+        }      
     }
 }
+}
+let click=boxes[].addEventListener('click',check);

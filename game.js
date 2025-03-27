85e6b7c26f1f1048105178b174ea7f71d5d61a84
HatimTaj <hatimtaj5306@gmail.com>
2025-03-27 22:51:36 +0530

    Tic-Tac-Toe completed

--- game.js
+++ game.js
@@ -23,6 +23,7 @@ boxes.forEach((box)=>{
                 boxes.disabled=true;
             }    
         }
+        checkWin();
     }) 
 })
 
@@ -32,4 +33,35 @@ reset.addEventListener("click",()=>{
     })
     status.innerHTML="X's turn";
     turn="X";
+    
+    boxes.forEach((box)=>{
+        box.disabled=false;
+        box.style.backgroundColor="#003049";
+    })
 })
+
+const winningPatterns =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]    
+]
+let checkWin = () => {
+    for(let i=0;i<winningPatterns.length;i++){
+        const [a,b,c] = winningPatterns[i];
+        if(boxes[a].innerHTML === boxes[b].innerHTML && boxes[a].innerHTML === boxes[c].innerHTML && boxes[a].innerHTML !== ""){
+            status.innerHTML = `${boxes[a].innerHTML} wins!`;
+            boxes[a].style.backgroundColor="green";
+            boxes[b].style.backgroundColor="green";
+            boxes[c].style.backgroundColor="green";
+            boxes.forEach((box)=>{
+                box.disabled=true;
+            })
+
+        }
+    }
+}
